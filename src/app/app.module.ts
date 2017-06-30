4ef613c16dc754169773ebad9304c53c5e11d055
Paul Proell <paul.proell@hotmail.com>
2017-06-30 08:54:19 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav.component';
+import {DashboardComponent} from './dashboard.component';
+import {DokumenteComponent} from './dokumente.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dokumente', component: DokumenteComponent },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    DokumenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dokumente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dokumente-component',
+  templateUrl: './dokumente.component.html',
+  styleUrls: ['./dokumente.component.css']
+})
+export class DokumenteComponent {
+  
+}
